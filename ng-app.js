b2c586e3b0f510305a64cce90649283850df892a
Andreas Larsson <andreas@chromawoods.com>
2015-11-15 18:19:58 +0100

    Added GUI draft

---
+++ ng-app.js
@@ -0,0 +1,41 @@
+var mimicryApp = angular.module('mimicryApp', ['ngAnimate']);
+
+
+mimicryApp.factory('languageData', ['$http', function($http) {
+
+  var cache = {};
+
+  return function(langId) {
+    if (!cache.hasOwnProperty(langId)) {
+      cache[langId] = $http.get('language-data/data-' + langId + '.json').success;
+    }
+    return cache[langId];
+  };
+
+}]);
+
+
+mimicryApp.controller('userInput', ['languageData', '$scope', '$rootScope', function(languageData, $scope, $rootScope) {
+
+  $scope.langId = 'fi';
+
+  $scope.getParagraphs = function(howMany) {
+    languageData($scope.langId)(function(data) {
+      $rootScope.paragraphs = mimicry.composer.registerLanguage(data).getParagraphs(howMany);
+    });
+  };
+
+}]);
+
+
+mimicryApp.controller('paragraphList', ['$scope', '$rootScope', function($scope, $rootScope) {
+
+  $scope.paragraphs = [];
+
+  $rootScope.$watch('paragraphs', function(newValue, oldValue) {
+    if (newValue !== oldValue) {
+      $scope.paragraphs = newValue;
+    }
+  });
+
+}]);
